b10e638671c17c38011049bbfc0a78e2b06d79e8
Sindhu Budhavarapu <sindhubudhavarapu@Sindhus-Air.fios-router.home>
2022-03-11 09:23:26 -0500

    models

--- iam/users/admin.py
+++ iam/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import newUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
 
 # Register your models here.
+class UserAdminConfig(UserAdmin):
+    model = newUser
+    ordering = ('start_date',)
+    list_display = ('email', 'username', 'firstname', 'lastname', 'is_active', 'is_staff')
+
+    admin.sit.register(newUser, UserAdminConfig)
